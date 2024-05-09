06435b478e203d8b39837b5fbc31e5474b8889f6
Minseo Kang <63099769+unvsDev@users.noreply.github.com>
2024-05-09 13:52:46 +0900

    feat: mui material 의존성

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { ReactElement, useState } from "react";
 
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
 export const MainPage = (): ReactElement => {
   return (
     <>
-      
+
     </>
   )
 }
